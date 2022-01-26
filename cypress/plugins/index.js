84e30fbf1083d1030577a2e7a6a0a8f945d9af65
orian <77785778+OrianP@users.noreply.github.com>
2022-01-26 14:22:26 +0000

    Edit file path in execFileSync to populate_db file
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,10 @@
 const { execFileSync } = require("child_process");
 
 module.exports = (on, config) => {
-    on("task", {
-      resetDb: () => {
-        console.log("Resetting DB...");
-        return execFileSync("./scripts/repopulate_db");
-      },
-    });
-  };
+	on("task", {
+		resetDb: () => {
+			console.log("Resetting DB...");
+			return execFileSync("./scripts/populate_db");
+		},
+	});
+};
