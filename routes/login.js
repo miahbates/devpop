f2752610fae644e56d270901fd428c35afa39d84
orian <77785778+OrianP@users.noreply.github.com>
2022-01-27 16:04:27 +0000

    Add logo div and move the 'back to home' anchor tag inside section
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,7 @@
 const auth = require("../auth.js");
 
 function get(request, response) {
-  const html = `
+	const html = `
     <!DOCTYPE html>
     <html lang="en">
         <head>
@@ -13,6 +13,7 @@ function get(request, response) {
         </head>
         <body>
             <section class="flex">
+                <div class="logo"></div>
                 <h1>Log in</h1>
                 <form action="/login" method="POST" class="flex">
                     <label for="email">Email<span aria-hidden="true">*</span></label>
@@ -26,35 +27,36 @@ function get(request, response) {
                         required
                     />
                     <button type="submit" class="btn">Log in</button>
+                    <a id="back-home" href="/" class="btn">Back to home</a>
                 </form>
-                <a id="back-home" href="/" class="btn">Back to home</a>
+                
             </section>
             
         </body>
     </html>`;
 
-  response.send(html);
+	response.send(html);
 }
 
 function post(request, response) {
-  // collect user input from request body
-  const { email, password } = request.body;
-  auth
-    // verify hashed password matches password input
-    // call model.getUser to retrieve user from db based on user email
-    .verifyUser(email, password)
-    // generates sid and calls model.createSession
-    // insert sid and user data into sessions table and
-    // returns sid column of sessions table
-    .then(auth.saveUserSession)
-    .then((sid) => {
-      response.cookie("sid", sid, auth.COOKIE_OPTIONS);
-      response.redirect("/newsfeed");
-    })
-    .catch((error) => {
-      console.error(error);
-      response.send("<h1>User not found</h1>");
-    });
+	// collect user input from request body
+	const { email, password } = request.body;
+	auth
+		// verify hashed password matches password input
+		// call model.getUser to retrieve user from db based on user email
+		.verifyUser(email, password)
+		// generates sid and calls model.createSession
+		// insert sid and user data into sessions table and
+		// returns sid column of sessions table
+		.then(auth.saveUserSession)
+		.then((sid) => {
+			response.cookie("sid", sid, auth.COOKIE_OPTIONS);
+			response.redirect("/newsfeed");
+		})
+		.catch((error) => {
+			console.error(error);
+			response.send("<h1>User not found</h1>");
+		});
 }
 
 module.exports = { get, post };
--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,7 @@
 const auth = require("../auth.js");
 
 function get(request, response) {
-  const html = `<!DOCTYPE html>
+	const html = `<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
@@ -12,6 +12,7 @@ function get(request, response) {
 	</head>
 	<body>
 		<section class="flex">
+			<div class="logo"></div>
 			<h1>Sign up</h1>
 			<form action="/signup" method="POST" class="flex">
 				<label for="name">Username<span aria-hidden="true">*</span></label>
@@ -34,36 +35,37 @@ function get(request, response) {
 					required
 				/>
 				<button type="submit" class="btn">Sign up</button>
+				<a id="back-home" href="/" class="btn">Back to home</a>
 			</form>
-			<a id="back-home" href="/" class="btn">Back to home</a>
+			
 		</section>
 		
 	</body>
 </html>`;
 
-  response.send(html);
+	response.send(html);
 }
 
 function post(request, response) {
-  // collect user input from request body
-  const { name, email, password } = request.body;
-  console.log("post request body", request.body);
-  // hash the password and call model.createUserDB
-  // insert user into db and return the first row of db table (name, email, hash)
-  auth
-    .createUser(name, email, password)
-    // generates sid and calls model.createSession
-    // insert sid and user data into sessions table and returns sid column of sessions table
-    .then(auth.saveUserSession)
-    // set a cookie with the name 'sid' and value of sid and pass cookie options
-    .then((sid) => {
-      response.cookie("sid", sid, auth.COOKIE_OPTIONS);
-      response.redirect("/login");
-    })
-    .catch((error) => {
-      console.error(error);
-      response.send(`<h1>Something went wrong, sorry</h1>`);
-    });
+	// collect user input from request body
+	const { name, email, password } = request.body;
+	console.log("post request body", request.body);
+	// hash the password and call model.createUserDB
+	// insert user into db and return the first row of db table (name, email, hash)
+	auth
+		.createUser(name, email, password)
+		// generates sid and calls model.createSession
+		// insert sid and user data into sessions table and returns sid column of sessions table
+		.then(auth.saveUserSession)
+		// set a cookie with the name 'sid' and value of sid and pass cookie options
+		.then((sid) => {
+			response.cookie("sid", sid, auth.COOKIE_OPTIONS);
+			response.redirect("/login");
+		})
+		.catch((error) => {
+			console.error(error);
+			response.send(`<h1>Something went wrong, sorry</h1>`);
+		});
 }
 
 module.exports = { get, post };
