5599dd6e923c68cca62767c0c11d7c557f2c85e4
neprojo <53294140+neprojo@users.noreply.github.com>
2022-01-27 12:34:15 +0000

    set up login post authentication
    Co-authored-by: Miah <78933903+miahbates@users.noreply.github.com>
    Co-authored-by: Milly <62570785+millipede-cpu@users.noreply.github.com>
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>
    Co-authored-by: Orian <77785778+OrianP@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -1,25 +1,37 @@
 const db = require("./connection.js");
 
 function createUserDB(name, email, hash) {
-	const INSERT_USER = `
+  const INSERT_USER = `
     INSERT INTO devpop_users (name, email, password) VALUES ($1, $2, $3)
     RETURNING id, name, email
   `;
-	return db.query(INSERT_USER, [name, email, hash]).then((result) => {
-		console.log('createUserDb', result.rows[0]);
-		result.rows[0];
-	});
+  return db.query(INSERT_USER, [name, email, hash]).then((result) => {
+    console.log("createUserDb", result.rows[0]);
+    return result.rows[0];
+  });
 }
 
 function createSession(sid, data) {
-	const INSERT_SESSION = `INSERT INTO sessions
+  const INSERT_SESSION = `INSERT INTO sessions
     (sid, data) VALUES ($1, $2) RETURNING sid`;
-	return db.query(INSERT_SESSION, [sid, data]).then((result) => {
-		console.log(result.rows[0].sid);
-		result.rows[0].sid;
-	});
+  return db.query(INSERT_SESSION, [sid, data]).then((result) => {
+    console.log("create sid", result.rows[0].sid);
+    return result.rows[0].sid;
+  });
+}
+
+function getUser(email) {
+  //SQL command to select ID, email, password and name
+  //from devpop users table where email matches user input
+  const SELECT_USER = `
+  SELECT id, name, email, password FROM devpop_users WHERE email = $1`;
+  //santizes email to stop SQL injection checks DB for email
+  return db.query(SELECT_USER, [email]).then((result) => {
+    console.log("getUser", result.rows[0]);
+    return result.rows[0];
+  });
 }
 
 // createUser("Holly", "email@fake.com", "1234")
 
-module.exports = { createUserDB, createSession };
+module.exports = { createUserDB, createSession, getUser };
