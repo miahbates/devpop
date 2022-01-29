753bbc9ac48fc854b207f84035d302e1fb1d7732
miahbates <miahbates@icloud.com>
2022-01-29 18:12:43 +0000

    able to login and go to homepage only if logges in changed sign-up to redirect to newfeed

--- routes/newsfeed.js
+++ routes/newsfeed.js
@@ -6,96 +6,115 @@ const db = require("../database/connection.js");
 const model = require("../database/model.js");
 
 function get(request, response) {
-	db.query("SELECT * FROM products").then((result) => {
-		const items = result.rows;
+  db.query("SELECT * FROM products").then((result) => {
+    const items = result.rows;
 
-		// console.log("items added",items);
-		const itemsList = items
-			.map(
-				(item) =>
-					`<li class="item">
-      <div>
-        <h2 class="item-title">${item.title}</h2>
-        <p>Type: ${item.product_type}</p>
-        <p>Description: ${item.description}</p>
-        <p>£${item.price}</p>
-      </div>
-    </li>`
-			)
-			.reverse()
-			.join("");
+    // get cookie
+    const sid = request.signedCookies.sid;
+    if (sid) {
+      // get session return data object
+      model.getSession(sid).then((data) => {
+        console.log(data);
+        // get data object, then user object and access id key.
+        const userId = data.user.id;
 
-		response.send(`<!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" type="text/css" href="./style.css" />
-        <title>DevPop</title>
-    </head>
-    <body>
-        <section>
-            
-            <section id="news-feed-container" class="flex">
-            <div class="logo"></div>
-            <h1>DevPop Feed</h1>
-            <p>Add your item</p>
-            <form action="/newsfeed" method="POST" id="add-item" class="flex">
+        // console.log("items added",items);
+        const itemsList = items
+          .map((item) => {
+            if (userId === item.seller_id) {
+              return `<li class="item">
+              <div>
+                <h2 class="item-title">${item.title}</h2>
+                delete button
+                <p>Type: ${item.product_type}</p>
+                <p>Description: ${item.description}</p>
+                <p>£${item.price}</p>
+              </div>
+            </li>`;
+            } else {
+              return `<li class="item">
+              <div>
+                <h2 class="item-title">${item.title}</h2>
+                <p>Type: ${item.product_type}</p>
+                <p>Description: ${item.description}</p>
+                <p>£${item.price}</p>
+              </div>
+            </li>`;
+            }
+          })
+          .reverse()
+          .join("");
 
-             <label for="name">Username<span aria-hidden="true">*</span></label>
-             <input type="text" name="name" required />
-
-              <label for="title">Name of item<span aria-hidden="true">*</span></label>
-              <input type="text" name="title" required />
-  
-              <label for="product_type">Choose product type<span aria-hidden="true">*</span></label>
-              <select id="product_type" name="product_type" form="add-item" required>
-                <option value="trousers">Trousers</option>
-                <option value="top">Tops</option>
-                <option value="dress">Dresses</option>
-                <option value="footwear">Footwear</option>
-                <option value="accessories">Accessories</option>
-                <option value="other">Other</option>
-              </select>
-  
-              <label for="description">Description of item<span aria-hidden="true">*</span></label>
-              <input
-                type="text"
-                name="description"
-                required
-              />
-  
-              <label for="price">Price<span aria-hidden="true">*</span></label>
-              <input
-                type="number"
-                name="price"
-                required
-              />            <button type="submit" id="add-item-btn" class="btn">Add item</button>
-            </form>  
-            <ul class="item-list">${itemsList}</ul>    
-            </section>
-    </body>
-  </html>`);
-	});
+        response.send(`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="stylesheet" type="text/css" href="./style.css" />
+      <title>DevPop</title>
+  </head>
+  <body>
+      <section>
+          
+          <section id="news-feed-container" class="flex">
+          <div class="logo"></div>
+          <h1>DevPop Feed</h1>
+          <p>Add your item</p>
+          <form action="/newsfeed" method="POST" id="add-item" class="flex">
+           <label for="name">Username<span aria-hidden="true">*</span></label>
+           <input type="text" name="name" required />
+            <label for="title">Name of item<span aria-hidden="true">*</span></label>
+            <input type="text" name="title" required />
+            <label for="product_type">Choose product type<span aria-hidden="true">*</span></label>
+            <select id="product_type" name="product_type" form="add-item" required>
+              <option value="trousers">Trousers</option>
+              <option value="top">Tops</option>
+              <option value="dress">Dresses</option>
+              <option value="footwear">Footwear</option>
+              <option value="accessories">Accessories</option>
+              <option value="other">Other</option>
+            </select>
+            <label for="description">Description of item<span aria-hidden="true">*</span></label>
+            <input
+              type="text"
+              name="description"
+              required
+            />
+            <label for="price">Price<span aria-hidden="true">*</span></label>
+            <input
+              type="number"
+              name="price"
+              required
+            />            <button type="submit" id="add-item-btn" class="btn">Add item</button>
+          </form>  
+          <ul class="item-list">${itemsList}</ul>    
+          </section>
+  </body>
+</html>`);
+      });
+    } else {
+      response.redirect("/");
+    }
+  });
 }
 
 function post(request, response) {
-	// collect user input from request body
-	// console.log(request.body);
-	// console.log(request.body.title);
-	// const title = request.body.title;
-	const { name, title, product_type, description, price } = request.body;
-  
+  // collect user input from request body
+  // console.log(request.body);
+  // console.log(request.body.title);
+  // const title = request.body.title;
+  const { name, title, product_type, description, price } = request.body;
+
   model
-  .addItem(name, title, product_type, description, price)
-  .then((result) => {
-    response.redirect("/newsfeed")
-  })
-  .catch((error) => {
-    console.log(error);
-    response.redirect("/");
-  });
+    .addItem(name, title, product_type, description, price)
+    .then((result) => {
+      response.redirect("/newsfeed");
+    })
+    .catch((error) => {
+      console.log(error);
+      response.redirect("/");
+    });
 }
 
 module.exports = { get, post };
